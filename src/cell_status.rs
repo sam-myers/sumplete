d040470995740a70dfa38f27b82f5ae95271ab21
Sam Myers <sam.myers@protonmail.com>
2023-03-13 19:49:37 -0500

    feat: add cell group

---
+++ src/cell_status.rs
@@ -0,0 +1,29 @@
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub enum CellStatus {
+    Unknown,
+    Known,
+    Removed
+}
+
+impl fmt::Display for CellStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            CellStatus::Unknown => write!(f, "?"),
+            CellStatus::Known => write!(f, "O"),
+            CellStatus::Removed => write!(f, "X"),
+        }
+    }
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_print() {
+        assert_eq!(format!("{}", CellStatus::Unknown), "?");
+        assert_eq!(format!("{}", CellStatus::Known), "O");
+        assert_eq!(format!("{}", CellStatus::Removed), "X");
+    }
+}
